c2290653162c82ee367a7a4bb68f3ec86c1c342b
madej-lukasz <madel@interia.pl>
2018-11-13 23:11:25 +0100

    Calculator

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class Calculator {
+    public void someMethod() {
+
+    }
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+        System.out.println("It works!");
+    }
+
+}
